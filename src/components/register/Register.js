1c808c0c55f45e097d2e8fdaac0845f1ba07d3be
Patrik Szepesi <patrikszepesi@gmail.com>
2019-02-21 17:26:44 +0100

    LA7

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -46,7 +46,7 @@ export class Register extends React.Component {
 
             </div>
           </div>
-            <div className='col-md-15'>
+            <div className='col-md-1'>
 
               <h1>Regisztráció</h1>
               <h6>Nem kell Tanárnak lenned,hogy Oktass!</h6>
